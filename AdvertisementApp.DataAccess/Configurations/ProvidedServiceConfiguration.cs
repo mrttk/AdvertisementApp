fa65e0fb2fc23b4309df2c6954f672655ebbc5ff
emre.tetik <emre.tetik@elekse.com>
2024-01-25 22:49:03 +0300

    add ProvidedServiceConfiguration

---
+++ AdvertisementApp.DataAccess/Configurations/ProvidedServiceConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using AdvertisementApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisementApp.DataAccess.Configurations
+{
+    public class ProvidedServiceConfiguration : IEntityTypeConfiguration<ProvidedService>
+    {
+        public void Configure(EntityTypeBuilder<ProvidedService> builder)
+        {
+            builder.Property(p => p.Title).HasMaxLength(300).IsRequired();
+            builder.Property(p => p.Description).HasColumnType("ntext").IsRequired();
+            builder.Property(p => p.ImagePath).HasMaxLength(500).IsRequired();
+            builder.Property(p => p.CreatedDate).HasDefaultValueSql("getdate()").IsRequired();
+        }
+    }
+}
