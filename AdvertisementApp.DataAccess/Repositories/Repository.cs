002a11a63541cb5402ea88b6739d879ed33ef457
emre.tetik <emre.tetik@elekse.com>
2024-01-28 13:50:45 +0300

    Add other repository operations

--- AdvertisementApp.DataAccess/Repositories/Repository.cs
+++ AdvertisementApp.DataAccess/Repositories/Repository.cs
@@ -7,12 +7,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Net.Security;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AdvertisementApp.DataAccess.Repositories
 {
-    public class Repository<T> : IRepository<T> where T : BaseEntity
+    public class Repository<T> : IRepository<T> where T : BaseEntity, IRepository<T>
     {
         private readonly AdvertisementContext _context;
 
@@ -45,6 +46,38 @@ namespace AdvertisementApp.DataAccess.Repositories
             return orderByType == OrderByType.ASC ?
                 await _context.Set<T>().Where(filter).AsNoTracking().OrderBy(selector).ToListAsync() :
                 await _context.Set<T>().Where(filter).AsNoTracking().OrderByDescending(selector).ToListAsync();
+        }
+
+        public async Task<T> FindAsync(object id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<T> GetByFilterAsync(Expression<Func<T, bool>> filter, bool asNoTracking = false)
+        {
+            return !asNoTracking ?
+                await _context.Set<T>().AsNoTracking().SingleOrDefaultAsync(filter) :
+                await _context.Set<T>().SingleOrDefaultAsync(filter);
+        }
+
+        public IQueryable<T> GetQuery()
+        {
+            return _context.Set<T>().AsQueryable();
+        }
+
+        public void Remove(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public async Task CreateAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+        }
+
+        public void Update(T entity, T unchanged)
+        {
+            _context.Entry(unchanged).CurrentValues.SetValues(entity);
         }
     }
 }
