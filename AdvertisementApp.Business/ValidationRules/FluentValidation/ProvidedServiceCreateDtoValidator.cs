b918e8cc96251bb38e9b85d9aab8d664e3986d85
emre.tetik <emre.tetik@elekse.com>
2024-01-30 23:52:31 +0300

    add validators

---
+++ AdvertisementApp.Business/ValidationRules/FluentValidation/ProvidedServiceCreateDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using AdvertisementApp.Dtos.ProvidedServiceDtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisementApp.Business.ValidationRules.FluentValidation
+{
+    public class ProvidedServiceCreateDtoValidator : AbstractValidator<ProvidedServiceCreateDto>
+    {
+        public ProvidedServiceCreateDtoValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty();
+            RuleFor(x => x.Description).NotEmpty();
+            RuleFor(x => x.ImagePath).NotEmpty();
+        }
+    }
+}
