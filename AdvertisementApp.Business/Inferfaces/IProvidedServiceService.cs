96268ab2aed3978625a712631bae43905d7f61cc
emre.tetik <emre.tetik@elekse.com>
2024-02-12 23:00:01 +0300

    add IProvidedServiceService

---
+++ AdvertisementApp.Business/Inferfaces/IProvidedServiceService.cs
@@ -0,0 +1,14 @@
+﻿using AdvertisementApp.Dtos.ProvidedServiceDtos;
+using AdvertisementApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisementApp.Business.Inferfaces
+{
+    public interface IProvidedServiceService : IService<ProvidedServiceCreateDto, ProvidedServiceUpdateDto, ProvidedServiceListDto, ProvidedService>
+    {
+    }
+}
