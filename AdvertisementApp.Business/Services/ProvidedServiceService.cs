ab670ff6e5617144807212e990157383b99020fa
emre.tetik <emre.tetik@elekse.com>
2024-02-12 23:00:14 +0300

    add ProvidedServiceService

---
+++ AdvertisementApp.Business/Services/ProvidedServiceService.cs
@@ -0,0 +1,22 @@
+﻿using AdvertisementApp.Business.Inferfaces;
+using AdvertisementApp.DataAccess.UnitOfWork;
+using AdvertisementApp.Dtos.ProvidedServiceDtos;
+using AdvertisementApp.Entities;
+using AutoMapper;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisementApp.Business.Services
+{
+    public class ProvidedServiceService : Service<ProvidedServiceCreateDto, ProvidedServiceUpdateDto, ProvidedServiceListDto, ProvidedService>, IProvidedServiceService
+    {
+        public ProvidedServiceService(IMapper mapper, IValidator<ProvidedServiceCreateDto> createDtoValidator, IValidator<ProvidedServiceUpdateDto> updateDtoValidator, IValidator<ProvidedServiceListDto> listDtoValidator, IUow uow) : base(createDtoValidator, updateDtoValidator, listDtoValidator, mapper, uow)
+        {
+
+        }
+    }
+}
