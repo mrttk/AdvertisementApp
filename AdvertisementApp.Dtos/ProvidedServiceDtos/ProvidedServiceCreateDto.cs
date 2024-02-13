1034cc04b351b7b2d2ea9e432dd30d92c5293460
emre.tetik <emre.tetik@elekse.com>
2024-02-13 23:48:16 +0300

    update providedServiceCreateDto

--- AdvertisementApp.Dtos/ProvidedServiceDtos/ProvidedServiceCreateDto.cs
+++ AdvertisementApp.Dtos/ProvidedServiceDtos/ProvidedServiceCreateDto.cs
@@ -9,10 +9,8 @@ namespace AdvertisementApp.Dtos.ProvidedServiceDtos
 {
     public class ProvidedServiceCreateDto : IDto
     {
-        public  int Id { get; set; }
         public string Title { get; set; }
         public string ImagePath { get; set; }
         public string Description { get; set; }
-        public DateTime CreatedDate { get; set; }
     }
 }
